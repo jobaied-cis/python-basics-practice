95479bba5f62818fbd1ff3e68f6f1afaaff77cf1
Jobaied Chowdhury <jobaiedchowdhuryakash031@gmail.com>
2026-07-01 18:54:03 +0600

    Added module in python

---
+++ extention_install.py
@@ -0,0 +1,9 @@
+import pyttsx3
+engine = pyttsx3.init()
+
+# For Mac, If you face error related to "pyobjc" when running the `init()` method :
+# Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
+
+engine.say("I will speak this text,I am Jobaied chowdhury, I am a python developer and I love to code in python")
+
+engine.runAndWait()
